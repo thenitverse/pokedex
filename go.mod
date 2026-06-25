3b278eea151346ffa2e72f860c3a858f26c4f026
nitika2205 <knitika567@gmail.com>
2026-06-25 18:07:43 +0530

    Add REPL with bufio scanner and input cleaning.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pokedex
+
+go 1.25.6
--- repl.go
+++ repl.go
@@ -1,6 +1,23 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		words := cleanInput(text)
+		fmt.Printf("Your command was: %s\n", words[0])
+
+	}
+}
 
 func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
