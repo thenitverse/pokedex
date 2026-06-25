0aa211900f98f1c09bbeb7915223fa964083905c
nitika2205 <knitika567@gmail.com>
2026-06-25 16:22:29 +0530

    implementing cleanInput helper and associated unit tests.

---
+++ repl_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    " hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "   Charmander  BulBasaur  PIKACHU   ",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("err")
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
+			}
+		}
+	}
+}
